82b4c31c6df199aa4c43399b74f9203ea603b6d7
Aingkaran <aingkaran1995@gmail.com>
2022-09-11 19:43:37 -0400

    Add final on click function and DOM manipulation

--- script.js
+++ script.js
@@ -2,25 +2,28 @@
 let longitude = 0
 let latitude =0
 let locationset = "toronto"
+const locationSearch = document.querySelector(".search-button")
+const locationText = document.querySelector(".search-bar")
+
+locationSearch.addEventListener('click', ()=>{
+    locationset=locationText.value
+    locationText.value= ""
+
 
 fetch('http://api.openweathermap.org/geo/1.0/direct?q='+locationset+'&limit=5&appid=eaae588901936a4177510a9a9982cbfe', {mode: 'cors'})
   .then(function (response) {
     return response.json()
   }).then(function (response) {
-    const cityName = document.querySelector(".city")
-    const cityTemp= document.querySelector(".Temperature")
-    const cityFeelsLike= document.querySelector(".feels-likeTemp")
-    const weather= document.querySelector(".weather")
-    const weatherDescription = document.querySelector(".weather-description")
-    const cityHumidity= document.querySelector(".humidity")
-    const citySearch = document.querySelector(".search-button")
-
-    citySearch.addEventListener('click', ()=>{
-
-        
+    const locationName = document.querySelector(".city")
+    const locationTemp= document.querySelector(".Temperature")
+    const locationFeelsLike= document.querySelector(".feels-likeTemp")
+    const locationweather= document.querySelector(".weather")
+    const locationweatherDescription = document.querySelector(".weather-description")
+    const locationHumidity= document.querySelector(".humidity")
 
 
-    })
+
+    
 
 
 
@@ -33,6 +36,13 @@ fetch('http://api.openweathermap.org/geo/1.0/direct?q='+locationset+'&limit=5&ap
         const response = await fetch('https://api.openweathermap.org/data/2.5/weather?lat='+latitude+'&lon='+longitude+'&appid=eaae588901936a4177510a9a9982cbfe&units=metric', {mode: 'cors'});
         const weatherData = await response.json();
         console.log(weatherData)
+
+        locationTemp.innerHTML= Math.round(weatherData.main.temp) +"&#8451;"
+        locationName.innerHTML= locationset.charAt(0).toUpperCase()+locationset.slice(1).toLowerCase()
+        locationFeelsLike.innerHTML ="Feels Like: "+ Math.round(weatherData.main.feels_like) +"&#8451;"
+        locationweather.innerHTML= "Weather Forecast: " + weatherData.weather[0].main;
+        locationweatherDescription.innerHTML = "Weather Description: " + weatherData.weather[0].description
+        locationHumidity.innerHTML = "Humidity: "+ weatherData.main.humidity +"%"
     
         console.log("Temperature: "+ weatherData.main.temp)
         console.log("Feels Like: "+ weatherData.main.feels_like)
@@ -41,10 +51,13 @@ fetch('http://api.openweathermap.org/geo/1.0/direct?q='+locationset+'&limit=5&ap
         console.log("Weather Description: "+ weatherData.weather[0].description)
       }
       getWeatherData(latitude,longitude);
+
+   
+
   }).catch(function (err) {
   })
 
-
+})
  
   
 
